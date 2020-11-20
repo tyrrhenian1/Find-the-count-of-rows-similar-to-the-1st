f456a083c07cb249079014bea13f4d6777bb99c7
Артем Лазарчик <lazarchik_2000@mail.ru>
2020-11-20 17:20:14 +0200

    123

--- matrica/MainWindow.xaml.cs
+++ matrica/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace matrica
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
